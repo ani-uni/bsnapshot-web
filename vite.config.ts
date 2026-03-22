04843a4df5e80eb097d44056698e44b896ea9990
rinne <180847792+rinnein@users.noreply.github.com>
2026-03-22 19:39:51 +0800

    build: remove vite-tsconfig-paths dependency and adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tailwindcss(), reactRouter()],
 })
