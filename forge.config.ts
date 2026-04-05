e438ebe8f0b72dbf48f915b1adf19b1edf0e5bd4
rinne <180847792+rinnein@users.noreply.github.com>
2026-04-05 23:46:37 +0800

    feat: enhance build and publish

--- forge.config.ts
+++ forge.config.ts
@@ -2,7 +2,11 @@ import { MakerDeb } from '@electron-forge/maker-deb'
 import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { MakerZIP } from '@electron-forge/maker-zip'
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives'
+import { FusesPlugin } from '@electron-forge/plugin-fuses'
+import { PublisherGithub } from '@electron-forge/publisher-github'
 import type { ForgeConfig } from '@electron-forge/shared-types'
+import { FuseV1Options, FuseVersion } from '@electron/fuses'
+import { MakerAppImage } from '@reforged/maker-appimage'
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -12,6 +16,11 @@ const config: ForgeConfig = {
       if (file === '/package.json') return false
       else if (file === '/LICENSE') return false
       else if (file.startsWith('/electron')) return false
+      else if (
+        file === '/node_modules' ||
+        file.startsWith('/node_modules/electron-squirrel-startup')
+      )
+        return false
       else if (file.startsWith('/build')) return false
       else if (file === '/backend' || file === '/backend/package.json')
         return false
@@ -21,12 +30,30 @@ const config: ForgeConfig = {
     appCategoryType: 'public.app-category.productivity',
     icon: 'assets/icon',
   },
+  plugins: [
+    new AutoUnpackNativesPlugin({}),
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+    }),
+  ],
   makers: [
     new MakerSquirrel({}, ['win32']),
-    new MakerZIP({}, ['darwin', 'linux']),
+    new MakerZIP({}, ['darwin']),
     new MakerDeb({}, ['linux']),
+    new MakerAppImage({}, ['linux']),
   ],
-  plugins: [new AutoUnpackNativesPlugin({})],
+  publishers: [
+    new PublisherGithub({
+      repository: {
+        owner: 'ani-uni',
+        name: 'BSnapshot',
+      },
+      draft: true,
+      prerelease: false,
+      generateReleaseNotes: true,
+    }),
+  ],
 }
 
 export default config
